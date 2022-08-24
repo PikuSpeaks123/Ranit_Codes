1571952f59d00c31ad8bffb409d727194a02dd9e
PikuSpeaks123 <103903179+PikuSpeaks123@users.noreply.github.com>
2022-08-24 19:35:02 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Hello !! \n");
+    printf("Enter a no. \n");
+    scanf("%d",&n);
+    printf("The no is %d \n",n);
+    return 0;
+    getch();
+}
